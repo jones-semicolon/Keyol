80f66f27b18ad57d74063738c2db988b22d22fdb
jonestly-source <marjones.lapinig@gmail.com>
2023-07-10 13:49:31 +0800

    Testable

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/images': "http://keyol.vercel.app",
+      '/images': "https://keyol.vercel.app/images",
     }
   },
   base: "/Keyol",
