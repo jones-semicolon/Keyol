dc4b27f1047ca927caaa89e00eac382b9899dda4
jonestly-source <marjones.lapinig@gmail.com>
2023-07-10 19:41:17 +0800

    test

--- server.js
+++ server.js
@@ -8,9 +8,7 @@ const app = express();
 app.use(express.json());
 dotenv.config();
 app.use((req,res,next)=>{
-    res.setHeader('Access-Control-Allow-Origin','*');
-    res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
-    res.setHeader('Access-Control-Allow-Methods','Content-Type','Authorization');
+    res.setHeader('Access-Control-Allow-Origin','https://jonestly-source.github.io');
     next(); 
 })
 
