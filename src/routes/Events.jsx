e186a71c5845fe038be1ccb91b6d547e80f1f440
jonestly-source <marjones.lapinig@gmail.com>
2023-07-16 20:01:23 +0800

    Arranged Environmental Variables

--- src/routes/Events.jsx
+++ src/routes/Events.jsx
@@ -1,69 +1,89 @@
-import { Component } from 'react'
-import { Content, Title, Gallery, Folder, TextOverlay } from "../styles/styled"
-import Loader from '../components/Loader'
-import PropTypes from 'prop-types'
+import { Component } from "react";
+import { Content, Title, Gallery, Folder, TextOverlay } from "../styles/styled";
+import Loader from "../components/Loader";
+import PropTypes from "prop-types";
 
-
-function RenderImage({ files, folders }){
-  const randomize = (length) => Math.floor(Math.random() * length)
+function RenderImage({ files, folders }) {
+  const randomize = (length) => Math.floor(Math.random() * length);
   let src = "";
-  if(files.length){
-    src = files[randomize(files.length)].thumbnailLink
+  if (files.length) {
+    src = files[randomize(files.length)].thumbnailLink;
   } else {
-    src = folders[randomize(folders.length)].files[files.length].thumbnailLink
+    src = folders[randomize(folders.length)].files[files.length].thumbnailLink;
   }
-  return <img src={src} alt="" loading="lazy"/>
+  return <img src={src} alt="" loading="lazy" />;
 }
 
 export default class Events extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       folder: [],
-      isLoaded: false
-    }
+      isLoaded: false,
+    };
   }
   initialize() {
     window.scrollTo(0, 0);
-    this.setState({ isLoaded: false })
-    if ('caches' in window) {
-      caches.open('folders').then((cache) => {
-        cache.match("events").then(resp => {
+    this.setState({ isLoaded: false });
+    if ("caches" in window) {
+      caches.open("folders").then((cache) => {
+        cache.match("events").then((resp) => {
           if (resp) {
-            resp.json().then(data => {
+            resp.json().then((data) => {
               if (Date.now() - data.timestamp < 5 * 60 * 1000) {
-                fetch(`https://keyol.vercel.app/images?folderId=${import.meta.env.VITE_FOLDER_ID}&folderPath=events&range=3`)
-                  .then(response => response.json())
+                fetch(
+                  `${import.meta.env.VITE_BASE_API}/images?folderId=${
+                    import.meta.env.VITE_FOLDER_ID
+                  }&folderPath=events&range=3`,
+                )
+                  .then((response) => response.json())
                   .then((res) => {
-                  const data = new Response(JSON.stringify({ folder: res.folders, timestamp: Date.now() }));
-                  cache.put("events", data);
-                  this.setState({ folder: res.folders, isLoaded: true })
-                }).catch((err) => {
-                  throw new Error(err)
-                })
+                    const data = new Response(
+                      JSON.stringify({
+                        folder: res.folders,
+                        timestamp: Date.now(),
+                      }),
+                    );
+                    cache.put("events", data);
+                    this.setState({ folder: res.folders, isLoaded: true });
+                  })
+                  .catch((err) => {
+                    throw new Error(err);
+                  });
+              } else {
+                this.setState({ folder: data.folder, isLoaded: true });
               }
-              else { this.setState({ folder: data.folder, isLoaded: true }); }
             });
           } else {
-            fetch(`https://keyol.vercel.app/images?folderId=${import.meta.env.VITE_FOLDER_ID}&folderPath=events&range=3`)
-              .then(response => response.json())
+            fetch(
+              `${import.meta.env.VITE_BASE_API}/images?folderId=${
+                import.meta.env.VITE_FOLDER_ID
+              }&folderPath=events&range=3`,
+            )
+              .then((response) => response.json())
               .then((res) => {
-              const data = new Response(JSON.stringify({ folder: res.folders, timestamp: Date.now() }));
-              cache.put("events", data);
-              this.setState({ folder: res.folders, isLoaded: true })
-            }).catch((err) => {
-              throw new Error(err)
-            })
+                const data = new Response(
+                  JSON.stringify({
+                    folder: res.folders,
+                    timestamp: Date.now(),
+                  }),
+                );
+                cache.put("events", data);
+                this.setState({ folder: res.folders, isLoaded: true });
+              })
+              .catch((err) => {
+                throw new Error(err);
+              });
           }
-        })
+        });
       });
     }
   }
   componentDidMount() {
-    this.initialize()
+    this.initialize();
   }
   render() {
-    const { folder, isLoaded } = this.state
+    const { folder, isLoaded } = this.state;
     return (
       <Content>
         {!isLoaded ? <Loader /> : undefined}
@@ -71,22 +91,21 @@ export default class Events extends Component {
           <h2>Events</h2>
         </Title>
         <Gallery className="gallery">
-          {
-            folder?.map(({ name, files, folders }, key) => {
-              return (
-                <Folder key={name} to={`/events/${name}`} delay={key}>
-                  <RenderImage files={files} folders={folders}/>
-                  <TextOverlay>{name}</TextOverlay>
-                </Folder>)
-            })
-          }
+          {folder?.map(({ name, files, folders }, key) => {
+            return (
+              <Folder key={name} to={`/events/${name}`} delay={key}>
+                <RenderImage files={files} folders={folders} />
+                <TextOverlay>{name}</TextOverlay>
+              </Folder>
+            );
+          })}
         </Gallery>
-      </Content >
-    )
+      </Content>
+    );
   }
 }
 
 RenderImage.propTypes = {
   files: PropTypes.object,
-  folders: PropTypes.object
-}
+  folders: PropTypes.object,
+};
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,53 +1,71 @@
-import { Component } from 'react'
-import { Content, Title, Gallery, Folder, TextOverlay } from "../styles/styled"
-import Loader from '../components/Loader'
+import { Component } from "react";
+import { Content, Title, Gallery, Folder, TextOverlay } from "../styles/styled";
+import Loader from "../components/Loader";
 export default class Home extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       folder: [],
       isLoaded: false,
-    }
+    };
   }
   initialize() {
     window.scrollTo(0, 0);
-    this.setState({ isLoaded: false })
-    if ('caches' in window) {
-      caches.open('folders').then((cache) => {
-        cache.match("home").then(resp => {
+    this.setState({ isLoaded: false });
+    if ("caches" in window) {
+      caches.open("folders").then((cache) => {
+        cache.match("home").then((resp) => {
           if (resp) {
-            resp.json().then(data => {
+            resp.json().then((data) => {
               if (Date.now() - data.timestamp < 5 * 60 * 1000) {
                 this.setState({ folder: data.folder, isLoaded: true });
-              }
-              else {
-                fetch(`https://keyol.vercel.app/images?folderId=${import.meta.env.VITE_FOLDER_ID}&range=3`)
-                  .then(response => response.json())
+              } else {
+                fetch(
+                  `${import.meta.env.VITE_BASE_API}/images?folderId=${
+                    import.meta.env.VITE_FOLDER_ID
+                  }&range=3`,
+                )
+                  .then((response) => response.json())
                   .then((res) => {
-                  const data = new Response(JSON.stringify({ folder: res.folders, timestamp: Date.now() }));
-                  cache.put("home", data);
-                  this.setState({ folder: res.folders, isLoaded: true })
-                }).catch((err) => {
-                  throw new Error(err)
-                })
+                    const data = new Response(
+                      JSON.stringify({
+                        folder: res.folders,
+                        timestamp: Date.now(),
+                      }),
+                    );
+                    cache.put("home", data);
+                    this.setState({ folder: res.folders, isLoaded: true });
+                  })
+                  .catch((err) => {
+                    throw new Error(err);
+                  });
               }
             });
           } else {
-            fetch(`https://keyol.vercel.app/images?folderId=${import.meta.env.VITE_FOLDER_ID}&range=3`)
+            fetch(
+              `${import.meta.env.VITE_BASE_API}/images?folderId=${
+                import.meta.env.VITE_FOLDER_ID
+              }&range=3`,
+            )
               .then((response) => response.json())
               .then((res) => {
-                console.log(res)
-                const data = new Response(JSON.stringify({ folder: res.folders, timestamp: Date.now() }));
+                console.log(res);
+                const data = new Response(
+                  JSON.stringify({
+                    folder: res.folders,
+                    timestamp: Date.now(),
+                  }),
+                );
                 cache.put("home", data);
-                this.setState({ folder: res.folders, isLoaded: true })
-              }).catch((err) => {
-                throw new Error(err)
+                this.setState({ folder: res.folders, isLoaded: true });
               })
+              .catch((err) => {
+                throw new Error(err);
+              });
           }
-        })
+        });
       });
     }
-
   }
   componentDidMount() {
     this.initialize();
@@ -55,30 +73,47 @@ export default class Home extends Component {
 
   componentDidUpdate(prevProps, prevState) {
     if (!prevState.folder && this.state.folder) {
-      this.initialize()
+      this.initialize();
     }
   }
   render() {
-    const { folder, isLoaded } = this.state
+    const { folder, isLoaded } = this.state;
     return (
       <Content>
         {!isLoaded ? <Loader /> : undefined}
         <Title>
-          <h3>My name is Caeoal Armentano and I am a passionate photographer dedicated to capturing the fleeting beauty of people.</h3>
+          <h3>
+            My name is Caeoal Armentano and I am a passionate photographer
+            dedicated to capturing the fleeting beauty of people.
+          </h3>
         </Title>
         <Gallery className="gallery">
-          {
-            folder?.map(({ name, files, folders }, key) => {
-              if (!name && !files.length) return
-              return (
-                <Folder key={key} to={`/${name}`} delay={key}>
-                  <img src={files.length ? `https://keyol.vercel.app/image?url=${encodeURIComponent(files[0].thumbnailLink)}` : `https://keyol.vercel.app/image?url=${encodeURIComponent(folders[0]?.files[0].thumbnailLink)}`} loading="lazy" />
-                  <TextOverlay>{name}</TextOverlay>
-                </Folder>)
-            })
-          }
+          {folder?.map(({ name, files, folders }, key) => {
+            if (!name && !files.length) return;
+            return (
+              <Folder key={key} to={`/${name}`} delay={key}>
+                <img
+                  src={
+                    files.length
+                      ? `${
+                          import.meta.env.VITE_BASE_API
+                        }/image?url=${encodeURIComponent(
+                          files[0].thumbnailLink,
+                        )}`
+                      : `${
+                          import.meta.env.VITE_BASE_API
+                        }/image?url=${encodeURIComponent(
+                          folders[0]?.files[0].thumbnailLink,
+                        )}`
+                  }
+                  loading="lazy"
+                />
+                <TextOverlay>{name}</TextOverlay>
+              </Folder>
+            );
+          })}
         </Gallery>
       </Content>
-    )
+    );
   }
 }
